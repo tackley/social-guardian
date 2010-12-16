6c4c7d11e8d1f1541a3e68c45681b6c60ba43a3f
Graham Tackley <graham@tackley.net>
2010-12-16 14:48:01 +0000

    Improved display for fronts

--- src/main/scala/net/tackley/sg/snippet/DisplayResults.scala
+++ src/main/scala/net/tackley/sg/snippet/DisplayResults.scala
@@ -2,11 +2,22 @@ package net.tackley.sg.snippet
 
 import net.tackley.sg.lib.Current
 import net.liftweb.util.Helpers._
+import com.gu.openplatform.contentapi.model.Content
+import xml._
 
 class DisplayResults {
 
-  def render =  "li *" #>  Current.item.results.map { item =>
-       ".link-text" #> <a href={"/"+item.id}>{item.webTitle}</a>
-     }
+  def bind(content: List[Content]) = "*" #> content.map { item =>
+    ".link-text" #> <a href={"/" + item.id}>{item.webTitle}</a> &
+    ".trail-text *" #> item.safeFields.get("trailText").map(Unparsed(_))
+  }
 
+  def col1 = bind(cols(0))
+  def col2 = bind(cols(1))
+  def col3 = bind(cols(2))
+
+
+  lazy val results = (Current.item.editorsPicks ++ Current.item.results).distinct
+  lazy val itemsPerCol = results.size / 3
+  lazy val cols = results.grouped(itemsPerCol).toList.padTo(3, Nil)
 }
