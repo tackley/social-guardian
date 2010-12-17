eafa6bac2e1dcb7ccccd733604be48cd20b0b029
Lisa van Gelder <lisa.van-gelder@guardian.co.uk>
2010-12-17 15:14:15 +0000

    Added recommendations

---
+++ src/main/scala/net/tackley/sg/snippet/Recommendations.scala
@@ -0,0 +1,32 @@
+package net.tackley.sg.snippet
+
+import com.gu.openplatform.contentapi.Api
+import com.gu.openplatform.contentapi.model.Content
+import net.tackley.sg.lib.Current
+import net.tackley.sg.model.User
+import net.liftweb.util.Helpers._
+import xml.{Unparsed}
+
+class Recommendations  {
+
+  lazy val latestContent: List[Content] =  Api.search.pageSize(50).showTags("keyword").response.results
+  lazy val myTags: Map[String, Int] = User.current.is.map(_.interestingTags.get) getOrElse Map()
+
+
+  lazy val scoredContent = latestContent.map(content => content -> scoreContent(content))
+    .sortBy { case (content, score) => score }.reverse.take(5)
+
+  def scoreContent (content : Content) = {
+    if (content.tags.isEmpty) 0 else {
+      val tagScores = for (tag <- content.tags) yield myTags.getOrElse(tag.id, 0)
+      tagScores.sum / content.tags.size
+    }
+  }
+
+  def render = "li *" #> scoredContent.map { case (content, score) =>
+    ".link-text" #> <a href={"/" + content.id}>{Unparsed(content.webTitle)}</a>
+  }
+
+
+
+}
