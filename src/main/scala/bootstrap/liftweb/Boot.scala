187f62c3dce017e2e6b5ade599ca93f269efc2cf
Graham Tackley <graham@tackley.net>
2010-12-20 10:31:54 +0000

    Comet refresh latest content on the front

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -12,7 +12,9 @@ import net.liftweb.json.JsonDSL._
 import net.liftweb.json.JsonAST._
 
 class Boot {
-  import Loc._
+
+  val nonGuPathRoots = "oauth" :: "index" :: LiftRules.cometPath :: LiftRules.ajaxPath :: Nil
+  val nonGuPathRootsOption = nonGuPathRoots.map(Some(_))
 
   def boot = {
     MongoDB.defineDbAuth(DefaultMongoIdentifier, MongoAddress(MongoHost("flame.mongohq.com", 27063), "socialguardian"), "bruntonspall", "1234567")
@@ -20,12 +22,12 @@ class Boot {
 
     LiftRules.htmlProperties.default.set((r: Req) => new Html5Properties(r.userAgent))
 
-   LiftRules.statelessRewrite.append {
-       case RewriteRequest(ParsePath(path, "", _, _), GetRequest, httpreq)
-         if !(List(Some("oauth"), Some("index"), Some("comet_request")) contains path.headOption) =>
-          println("rewriting " + path)
-          RewriteResponse("index" :: Nil, Map("path" -> path.mkString("/")))
-     }
+    LiftRules.statelessRewrite.append{
+      case RewriteRequest(ParsePath(path, "", _, _), GetRequest, httpreq)
+        if !(nonGuPathRootsOption contains path.headOption) =>
+        println("rewriting " + path)
+        RewriteResponse("index" :: Nil, Map("path" -> path.mkString("/")))
+    }
 
     LiftRules.addToPackages("net.tackley.sg")
 
@@ -41,22 +43,23 @@ class Boot {
 
   val logger = Logger(this.getClass)
 
-  def doSignin:Box[LiftResponse] = {
-    val request_token = OAuthLogin.requestRequestToken.get
-    S.set("oauth_request_token", request_token.value)
-    S.set("oauth_request_secret", request_token.secret)
-    return Full(RedirectResponse("http://api.twitter.com/oauth/authenticate?oauth_token="+request_token.value))
+  def doSignin: Box[LiftResponse] = {
+    for (request_token <- OAuthLogin.requestRequestToken) yield {
+      S.set("oauth_request_token", request_token.value)
+      S.set("oauth_request_secret", request_token.secret)
+      RedirectResponse("http://api.twitter.com/oauth/authenticate?oauth_token=" + request_token.value)
+    }
   }
 
-  
-  def doCallback:Box[LiftResponse] = {
+
+  def doCallback: Box[LiftResponse] = {
     val token = Token(S.param("oauth_token").get, S.get("oauth_request_secret").get)
     val verifier = S.param("oauth_verifier").get
     // Now we trade token and verifier for an actual access_token which we can store
     val access_token = OAuthLogin.requestAccessToken(token, verifier).get
     val userDetails = OAuthLogin.requestUserDetails(access_token).get
-    logger.debug("Access token = "+access_token)
-    logger.debug("User = "+userDetails \ "name")
+    logger.debug("Access token = " + access_token)
+    logger.debug("User = " + userDetails \ "name")
     val u = User.find("oauthAccessToken", access_token.value).getOrElse(User.createRecord)
     u.name(userDetails \ "screen_name" text)
     u.fullName(userDetails \ "name" text)
